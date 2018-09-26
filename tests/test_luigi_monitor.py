4e63932daf30051614bfbb8142dacc90818764a7
Sadiq Sahib <sadiq@growthintel.com>
2018-09-26 15:27:39 +0100

    Implemented test for send_flow_result and clean refactoring
    AXE-785 #time 40m

--- tests/test_luigi_monitor.py
+++ tests/test_luigi_monitor.py
@@ -1,16 +1,19 @@
 import httpretty
+import json
 import luigi
 import pytest
-import requests
-
 
 from luigi_monitor import luigi_monitor
 
 
-def test_luigi_monitor_format_message():
+@pytest.fixture
+def job():
+    return 'AnyDummyLuigiTask()'
+
+
+def test_luigi_monitor_format_message(job):
     """Test to verify format_message returns as expected"""
     max_print = 1
-    job = 'AnyDummyLuigiTask()'
     job_status_report, slack_message_attachment = luigi_monitor.format_message(max_print, job)
     assert job_status_report == "Status report for AnyDummyLuigiTask()"
     assert isinstance(slack_message_attachment, list)
@@ -48,16 +51,28 @@ def test_luigi_monitor_success_without_on_success_message(events):
     assert events['Success'][0]['task'] == 'DummyWithoutOnSuccessLuigiTask()'
 
 
+@pytest.fixture
+def slack_url():
+    return "https://test-gi-slack-url.com/test/flow/result"
 
 
-# @httpretty.activate
-# def test_send_flow_result():
-#     url = "https://test-gi-slack-url.com/test/flow/result"
-#     max_print = 2
-#     status_code = 200
-#     httpretty.register_uri(httpretty.GET, url, status=status_code)
-#     r = requests.get(url)
-#     assert r is not None
+@pytest.fixture
+def http_mock():
+    httpretty.enable()
+    yield httpretty
+    httpretty.disable()
+    httpretty.reset()
 
-def test_send_message():
-    pass
+
+@pytest.fixture
+def max_print():
+    return 1  # any random int will do
+
+
+def test_send_flow_result(http_mock, slack_url, job, max_print):
+    status_code = 200
+    http_mock.register_uri(httpretty.POST, slack_url, status=status_code)
+    luigi_monitor.send_flow_result(slack_url, max_print, job)
+    request_payload = json.loads(http_mock.last_request().body)
+    assert set(request_payload.keys()) == {'text', 'attachments'}
+
